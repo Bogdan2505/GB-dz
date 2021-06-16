a8e6403bbee0a792dcbe5ac91d02082f06f8b46d
Bogdan <bogbog680@gmail.com>
2021-06-16 11:41:56 +0300

    add class method printThreeWords and checkSumSign

--- src/dz/Main.java
+++ src/dz/Main.java
@@ -4,11 +4,25 @@ public class Main {
 
     public static void main(String[] args) {
         printThreeWords();
+        checkSumSign();
     }
 
     public static void printThreeWords() {
         System.out.println("Orange");
         System.out.println("Banana");
         System.out.println("Apple");
+    }
+
+    public static void checkSumSign() {
+        int a = 5;
+        int b = 2;
+        int c = a + b;
+
+        if (c >= 0) {
+            System.out.println("сумма положениительная  ");
+        } else {
+            System.out.println("сумма отрицицательная");
+        }
+
     }
 }
